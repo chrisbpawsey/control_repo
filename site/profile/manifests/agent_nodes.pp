fb5b38f57271bff6721cbb6d1fa847533c2700bd
Chris Bording <Chris.Bording@uwa.edu.au>
2020-06-04 12:21:55 +0800

    update agent_nodes puppet file

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,13 +3,13 @@ class profile::agent_nodes {
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
   dockeragent::node {'mine.puppet.vm':}
+
   host {'db.puppet.vm':
     ensure => present,
     ip     => '172.18.0.2'
-  }  
+  }
   host {'web.puppet.vm':
     ensure => present,
     ip     => '172.8.0.3'
   }
 }
-
